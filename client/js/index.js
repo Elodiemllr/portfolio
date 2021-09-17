1b71c6ef263ac573afae6a17d1f44d4439beaf86
elodiemllr <elodie.muller.pro@outlook.com>
2021-09-17 15:01:32 +0200

    toggle skills function

--- client/js/index.js
+++ client/js/index.js
@@ -1,41 +1,55 @@
-
 /*show menu */
-//on recupère notre element par son id 
-const 
-    navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+//on recupère notre element par son id
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
-
-//  si notre const existe 
-if(navToggle){
-    //on ajoute un écouteur, et au click sur notre toggle 
+//  si notre const existe
+if (navToggle) {
+    //on ajoute un écouteur, et au click sur notre toggle
     //on ajoute la class show-menu a notre navMenu qui permettra de le montrer
-   
 
-    navToggle.addEventListener('click', () => {
-navMenu.classList.add('show-menu')
-    })
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
 }
 
-if(navClose){
-     //on ajoute un écouteur, et au click sur notre close 
+if (navClose) {
+    //on ajoute un écouteur, et au click sur notre close
     //on enlève la class show-menu a notre navMenu qui permettra de cacher notre menu
-   
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    }
-    )
+
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
 }
 
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll(".nav__link");
 
 //on créer une function qui quand on click sur un nav link de notre menu, on cache le menu en lui enlevant sa classe
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
+    const navMenu = document.getElementById("nav-menu");
 
-    navMenu.classList.remove('show-menu')
+    navMenu.classList.remove("show-menu");
 }
 
 //on execute la fonction pour chacun de nox nav link
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+//up and down skills
+const skillsContent = document.getElementsByClassName("skills__content"),
+    skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    var i;
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = "skills__content skills__close";
+    }
+    if (itemClass === "skills__content skills__close") {
+        this.parentNode.className = "'skills__content skills__open";
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener("click", toggleSkills);
+});
