7e2ed61a2cede5ab8addf5ce0311e9fb2a723622
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-10-04 13:25:01 +0200

    adding cookie parser

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const nodemailer = require("nodemailer");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config({ path: "./config/.env" });
 
 const PORT = process.env.PORT || 5000;
-
+const maxAge = 3 * 24 * 60 * 60 * 1000;
 //ici on précise tout ce qu'on autorise
 
 app.use(cors());
 
 app.use(express.static("clientjs"));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/clientjs/index.html");
+});
+
+app.get("/setcookie", (req, res) => {
+    res.cookie(`Cookie token name`, `encrypted cookie string Value`, {
+        maxAge: 5000,
+        // expires works the same as the maxAge
+        expiresIn: maxAge,
+        secure: true,
+        httpOnly: true,
+        sameSite: "lax",
+    });
+    res.send("Cookie have been saved successfully");
+});
+
+app.get("/deletecookie", (req, res) => {
+    //show the saved cookies
+    res.clearCookie();
+    res.send("Cookie has been deleted successfully");
 });
 
 app.post("/", (req, res) => {
